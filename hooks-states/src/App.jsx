fa4e932585469324d805d5db1faa42db5e834c56
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-16 13:55:05 +0200

    ejercicio 1 terminado

--- hooks-states/src/App.jsx
+++ hooks-states/src/App.jsx
@@ -1,23 +1,16 @@
-import React from 'react'
-import LikeButton from './components/LikeButton'
-
+import React from "react";
+import LikeButton from "./components/LikeButton";
 
 const App = () => {
-
   return (
-    <div className="flex flex-col">
-      <p className="m-auto p-10">EJERCICIO 1</p>
-      <div>
-        <LikeButton/>
-      </div>
+    <div className="flex flex-col justify-center items-center">
+      <p className="p-10">EJERCICIO 1</p>
 
+      <LikeButton />
 
-
-
+      <p className="p-10">EJERCICIO 2</p>
     </div>
-  
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
--- hooks-states/src/components/LikeButton.jsx
+++ hooks-states/src/components/LikeButton.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const LikeButton = () => {
-    const [counter, setCounter] = useState(0);
-	const addLikes = () => setCounter(prev => prev += 1);
+  const bgColors = ["purple", "blue", "green", "yellow", "orange", "red"];
+
+  const [likes, setLikes] = useState(0);
+  const [color, setColor] = useState(0);
+  const addLikes = () => {
+    setLikes((prev) => (prev += 1));
+    setColor((prevIndex) => (prevIndex + 1) % bgColors.length)
+  };
+
   return (
     <div>
-        <button onClick={addLikes}> <span>{counter}</span> Likes</button>
-      
+      <button 
+      className={`bg-[${bgColors[color]}] w-36 h-10 flex items-center justify-center rounded text-xl`}
+      onClick={addLikes}
+      >
+        <span className="mr-1">{likes}</span> Likes
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default LikeButton
+
+
+export default LikeButton;
